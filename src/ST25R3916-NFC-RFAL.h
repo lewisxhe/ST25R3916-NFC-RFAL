c48e01023ff836a657cc092f1274a0eb8ec5aaa1
lewisxhe <lewishe@outlook.com>
2026-09-19 15:44:39 +0800

    Fix Arduino library registry metadata

---
+++ src/ST25R3916-NFC-RFAL.h
@@ -0,0 +1,16 @@
+/******************************************************************************
+ * ST25R3916-NFC-RFAL public entry point
+ *
+ * Include this header when an application needs the combined ST25R3916,
+ * RFAL, and NDEF APIs provided by this library.
+ ******************************************************************************/
+
+#ifndef ST25R3916_NFC_RFAL_H
+#define ST25R3916_NFC_RFAL_H
+
+#include "st25r3916.h"
+#include "st25r3916_arduino_bus.h"
+#include "rfal_nfc.h"
+#include "ndef_class.h"
+
+#endif /* ST25R3916_NFC_RFAL_H */
